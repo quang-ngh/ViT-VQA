4e66dd1558d4029efb032b4a5363eab9efac6c0d
nhq <nhqcs@pop-os.localdomain>
2022-01-04 10:00:21 +0700

    update main

--- main.py
+++ main.py
@@ -6,7 +6,5 @@ import random
 
 model = create_model()
 vinputs = np.random.rand(1,420,420,1)
-linputs = np.random.rand(1,random.randint(50,100), random.randint(50,100))
-voutputs, loutputs = model(vinputs, linputs)
-print(voutputs)
-print(loutputs)
+linputs = np.random.rand(1, random.randint(50,100))
+outputs = model(vinputs, linputs)

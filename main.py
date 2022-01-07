938c3a3d7de7fd08acdc7fd5b8ffe23f651ae0e7
nhq <nhqcs@pop-os.localdomain>
2022-01-08 01:17:50 +0700

    train tpu

--- main.py
+++ main.py
@@ -13,43 +13,44 @@ train_loss = []
 model = create_model()
 
 def train(model):
-    optimizer = tfa.optimizers.AdamW(learning_rate = 0.001, weight_decay = 1e-4)
-    loss_obj = tf.keras.losses.CategoricalCrossentropy()
-    dataset = get_data_train(trainDataSet, seqContactDict)
+    with tf.device("/TPU:0"):
+        optimizer = tfa.optimizers.AdamW(learning_rate = 0.001, weight_decay = 1e-4)
+        loss_obj = tf.keras.losses.CategoricalCrossentropy()
+        dataset = get_data_train(trainDataSet, seqContactDict)
 
-    for epoch in range(EPOCHS):
-        epoch_loss_avg = tf.keras.metrics.Mean()
-        print(epoch)
-        for batch, (lines, contactMap, proper) in enumerate(dataset):
-            """
-            Input to model: 
-            String: Smiles --> shape = [1,x]
-            Feature 2D: Contactmap --> Shape = [1, size, size, 1]
-            """
-            contactMap = np.reshape(contactMap, (1,contactMap.shape[1], contactMap.shape[-1],1))
-            contactMap_size = tf.shape(contactMap)[1]
-           
-            #Fixed: Pass a fixed size variabels
-            contactMap = tf.keras.layers.ZeroPadding2D(padding = ((0,800-contactMap_size), (0, 800-contactMap_size)), data_format = 'channels_last')(contactMap) 
-            smiles, length, y = make_variables([lines], proper, smiles_letters)
-            smiles = tf.reshape(smiles, [1, smiles.shape[-1]])
+        for epoch in range(EPOCHS):
+            epoch_loss_avg = tf.keras.metrics.Mean()
+            print(epoch)
+            for batch, (lines, contactMap, proper) in enumerate(dataset):
+                """
+                Input to model: 
+                String: Smiles --> shape = [1,x]
+                Feature 2D: Contactmap --> Shape = [1, size, size, 1]
+                """
+                contactMap = np.reshape(contactMap, (1,contactMap.shape[1], contactMap.shape[-1],1))
+                contactMap_size = tf.shape(contactMap)[1]
             
-            with tf.GradientTape() as tape:
-                logits = model(smiles, contactMap, training=True) 
+                #Fixed: Pass a fixed size variabels
+                contactMap = tf.keras.layers.ZeroPadding2D(padding = ((0,800-contactMap_size), (0, 800-contactMap_size)), data_format = 'channels_last')(contactMap) 
+                smiles, length, y = make_variables([lines], proper, smiles_letters)
+                smiles = tf.reshape(smiles, [1, smiles.shape[-1]])
                 
-                loss =loss_obj(y, logits)
+                with tf.GradientTape() as tape:
+                    logits = model(smiles, contactMap, training=True) 
+                    
+                    loss =loss_obj(y, logits)
 
-            grads = tape.gradient(loss, model.trainable_variables)
-        
-            optimizer.apply_gradients((grads, var) for (grads, var) in zip(grads, model.trainable_variables))
+                grads = tape.gradient(loss, model.trainable_variables)
             
-            epoch_loss_avg.update_state(loss)
-            if batch % 1000:
-                print("Loss: {} -- After {}".format(epoch_loss_avg.result(), batch))
+                optimizer.apply_gradients((grads, var) for (grads, var) in zip(grads, model.trainable_variables))
                 
-        train_loss.append(epoch_loss_avg.result())
-        plt.plot(train_loss)
-        plt.show()
+                epoch_loss_avg.update_state(loss)
+                if batch % 1000:
+                    print("Loss: {} -- After {}".format(epoch_loss_avg.result(), batch))
+                    
+            train_loss.append(epoch_loss_avg.result())
+            plt.plot(train_loss)
+            plt.show()
             
 train(model)   
 #datasets = get_data_train(trainDataSet, seqContactDict)

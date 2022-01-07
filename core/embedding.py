add padding & fixed gradients

core/embedding.py
@@ -70,9 +70,9 @@ class PatchesEmbedding(tf.keras.layers.Layer):
         #print("Shape {}".format(patches_encoded.shape))
         return patches_encoded
 
-class Smiles_Embedding(tf.keras.layers.Embedding):
+class Smiles_Embedding(tf.keras.layers.Layer):
     def __init__(self, n_char, hidden_dim):
-        super(Smiles_Embedding, self).__init__(n_char, hidden_dim)
+        super(Smiles_Embedding, self).__init__()
         self.n_char = n_char
         self.hidden_dim = hidden_dim
         self.embd = tf.keras.layers.Embedding(n_char, hidden_dim, name = "smiles_embd")
@@ -87,7 +87,9 @@ class Smiles_Embedding(tf.keras.layers.Embedding):
         input_shape = inputs.shape[1]
         inputs = tf.keras.layers.ZeroPadding1D(padding = (0,256-input_shape))(inputs)
         inputs = tf.reshape(inputs, (1,inputs.shape[1]))
-        print("Shape of string inp: {}".format(tf.shape(inputs)))
+        #print("Shape of string inp: {}".format(tf.shape(inputs)))
         output = self.embd(inputs)
-        output = tf.concat([self.s_cls_token, output], axis = 1)
+        tmp_cls = tf.cast(tf.broadcast_to(self.s_cls_token, [tf.shape(inputs)[0],1,tf.shape(output)[-1]]),
+                            dtype = tf.float32)
+        output = tf.concat([tmp_cls, output], axis = 1)
         return output

core/encode.py
@@ -41,13 +41,14 @@ class MHSABlock(tf.keras.layers.Layer):
         attention = self.attention(query = query, value = value, key = key) # Pass by Multihead attention
         
         attention += inputs #Skip connection
+        skip = attention
+        attention = self.layerNormFC(attention) # Pass by layer norm 
         
-        output = self.layerNormFC(attention) # Pass by layer norm 
-        output = self.FC(attention) # Pass by fully connected 
+        attention = self.FC(attention) # Pass by fully connected 
         
-        output += attention # Skip connection
+        attention += skip # Skip connection
 
-        return output
+        return attention
 
 class Encoder(tf.keras.layers.Layer):
     def __init__(self, num_layers_encoder, num_heads, Dim, hidden_dim, dropout, norm_coff = 1e-12):

core/model.py
@@ -37,8 +37,8 @@ class MHSADrugVQA(tf.keras.models.Model):
             tf.keras.layers.Dense(units = 2, activation = 'softmax')
         ]
         )
-        self.flatten = tf.keras.layers.Flatten()
-        self.dense = tf.keras.layers.Dense(units = 2, activation = 'softmax')
+        #self.flatten = tf.keras.layers.Flatten()
+        #self.dense = tf.keras.layers.Dense(units = 2, activation = 'softmax')
         #self.poolV = tf.keras.layers.MaxPool2D()
 
         
@@ -66,28 +66,29 @@ class MHSADrugVQA(tf.keras.models.Model):
         smiles = tf.Variable(smiles, dtype=tf.float32)
         contactMap = tf.Variable(contactMap, dtype = tf.float32)
         smiles = tf.reshape(smiles, (1,tf.shape(smiles)[-1],1))
-        
+
         v_embd = self.Vembedding(contactMap)
         l_embd = self.Lembedding(smiles)
-        
+        #print("Shape of inp V: {}".format(tf.shape(v_embd)))
+        #print("Shape of inp L: {}".format(tf.shape(l_embd)))
         img_rep = self.encoderV(v_embd) #Shape = [batch_size, Dim] 
         seq_rep = self.encoderL(l_embd)
         
-        img_vec = img_rep[:,0]#self.flatten(img_rep)
-        seq_vec = seq_rep[:, 0]#self.flatten(seq_rep)
+        #img_vec = img_rep[:,0]#self.flatten(img_rep)
+        #seq_vec = seq_rep[:, 0]#self.flatten(seq_rep)
         #img_vec = tf.reshape(tf.nn.max_pool1d(img_rep, ksize = (img_rep.shape[1]), strides = 1, padding = "VALID"), (1,-1)) #add batch_size 
         #seq_vec = tf.reshape(tf.nn.max_pool1d(seq_rep, ksize=(seq_rep.shape[1]), strides = 1, padding = "VALID"), (1,-1)) #add batch_size
         
 
-        #img_vec = img_rep[:, 0]
-        #seq_vec = seq_rep[:, 0]
-        print("Shape: {}".format(tf.shape(img_vec)))
-        print("Shape: {}".format(tf.shape(seq_vec)))
+        img_vec = img_rep[:, 0]
+        seq_vec = seq_rep[:, 0]
+        #print("Shape: {}".format(tf.shape(img_vec)))
+        #print("Shape: {}".format(tf.shape(seq_vec)))
         #mcb_output = self.MCB(img_vec, seq_vec)
         mcb_output = tf.concat([img_vec, seq_vec], axis = 1)
-        print("Inp shape : {}".format(tf.shape(mcb_output)))
+        #print("Inp shape : {}".format(tf.shape(mcb_output)))
         output = self.classifier(mcb_output)
-        #output = self.dense()
+        
 
         return output
 

main.py
@@ -6,13 +6,14 @@ import random
 from dataTF import *
 from utils import *
 import matplotlib.pyplot as plt
+import tensorflow_addons as tfa
 
 EPOCHS = 30
 train_loss = []
 model = create_model()
 
 def train(model):
-    optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+    optimizer = tfa.optimizers.AdamW(learning_rate = 0.001, weight_decay = 1e-4)
     loss_obj = tf.keras.losses.CategoricalCrossentropy()
     dataset = get_data_train(trainDataSet, seqContactDict)
 